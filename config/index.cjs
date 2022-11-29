fb911ed5b4ad7c41ceb2252c90cbd5d584450537
Ame-Zhang <82988748+Ame-Zhang@users.noreply.github.com>
2022-11-30 07:08:04 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小丁',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o87ad6S5slgwaLMsr2SKduTkCVQU',
+      id: 'o87ad6UXQO1gkbgAmXvn0kntre94',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'gysjz9KlvODa9_w3BCOKipe9sE5Y1tTCqjoYM_BKCvw',
 
@@ -51,7 +51,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小丁',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o87ad6S5slgwaLMsr2SKduTkCVQU',
+      id: 'o87ad6UXQO1gkbgAmXvn0kntre94',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'nIeDwWyPEP83OuDiTBuqXGKZ8GDSo1OXQDz7cYnG4uc',
     },
